4ff45a6f662e8d834a6dc535d7c8ff7a3745d56f
Anantdadhich <anantdadhich66@gmail.com>
2025-04-12 23:22:04 +0530

    added tokenlottery

---
+++ create-codeman-client.ts
@@ -0,0 +1,27 @@
+import { createFromRoot } from "codama";
+import { rootNodeFromAnchor } from "@codama/nodes-from-anchor";
+import { renderJavaScriptVisitor } from "@codama/renderers";
+import path from "path";
+import { promises as fs } from "fs";
+
+const loadJSON = async (...pathSegments: Array<string>) => {
+  const filePath = path.join(...pathSegments);
+  try {
+    return JSON.parse(await fs.readFile(filePath, "utf-8"));
+  } catch (error) {
+    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
+      throw new Error(`Failed to load JSON file: ${filePath} does not exist`);
+    }
+    throw error;
+  }
+};
+
+// Instantiate Codama
+//@ts-ignore
+const idl = await loadJSON("target", "idl", "escrow.json");
+
+const codama = createFromRoot(rootNodeFromAnchor(idl));
+
+// Render JavaScript.
+const generatedPath = path.join("dist", "js-client");
+codama.accept(renderJavaScriptVisitor(generatedPath));
--- programs/tokenlottery/src/lib.rs
+++ programs/tokenlottery/src/lib.rs
@@ -1,5 +1,12 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::spl_token_metadata_interface::instruction::Initialize;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{mint_to, Mint, MintTo, TokenAccount, TokenInterface}
+};
+use anchor_spl::metadata::{
+    Metadata,
+    
+};
 
 declare_id!("Fq7vmjuU79yXFSb8pPC14Mrw1yWH3rEPrPkWPhKbnMj7");
 
@@ -7,7 +14,7 @@ declare_id!("Fq7vmjuU79yXFSb8pPC14Mrw1yWH3rEPrPkWPhKbnMj7");
 pub mod token_lottery {
     use super::*;
 
-    pub fn configinitialize(ctx: Context<ConfigInitialize>,start:u64,end:u64,price:u64) -> Result<()> {
+    pub fn configinitialize(ctx: Context<InitializeConfig>,start:u64,end:u64,price:u64) ->Result<()> {
              ctx.accounts.token_lottery.lottery_start=start;
              ctx.accounts.token_lottery.lottery_end=end;
              ctx.accounts.token_lottery.price=price;
@@ -18,12 +25,40 @@ pub mod token_lottery {
              ctx.accounts.token_lottery.bump=ctx.bumps.token_lottery;
         Ok(())
     }
+   
 
+   pub fn lotteryinitalize(ctx:Context<InitializeLottery>)->Result<()>{
+       //now we create the collection
+
+         let signer_seeds:&[&[&[u8]]]=&[&[
+            b"collection_mint".as_ref(),
+            &[ctx.bumps.collection_mint],
+         ]];
+
+          msg!("Creating mint accounts") ;
+
+          mint_to(CpiContext::new_with_signer(
+           ctx.accounts.token_program.to_account_info(),
+           MintTo{
+            mint:ctx.accounts.collection_mint.to_account_info(),
+            to:ctx.accounts.collection_token_account.to_account_info(),
+            authority:ctx.accounts.collection_mint.to_account_info()
+           },
+           signer_seeds,
+
+          ) ,
+          1,
+        )?;
+
+
+        
+    Ok(())
+   }
 
 }
 
 #[derive(Accounts)]
-pub struct ConfigInitialize<'info> {
+pub struct InitializeConfig<'info> {
 
     #[account(mut)]
      pub payer:Signer <'info>,
@@ -34,12 +69,59 @@ pub struct ConfigInitialize<'info> {
         seeds=[b"token_lottery".as_ref()],
         bump
      )]
+
      pub token_lottery:Box<Account<'info,TokenLottery>> ,
 
 
      pub system_program:Program<'info,System>
 }
+ 
 
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+    #[account(mut)]
+   pub payer:Signer<'info> ,
+   #[account(
+     init,
+     payer=payer,
+     mint::decimals=0,
+     mint::authority=collection_mint,
+     mint::freeze_authority=collection_mint,
+    seeds=[b"collection_mint".as_ref()],
+    bump
+)]
+
+   pub collection_mint:Box<InterfaceAccount<'info,Mint>>,
+
+  /// CHECK: This account will be initialized by the metaplex program
+   #[account(mut)]
+   pub metadata:UncheckedAccount<'info> ,
+  /// CHECK: This account will be initialized by the metaplex program
+   #[account(mut)] 
+   pub master_edition:UncheckedAccount<'info>,
+
+      #[account(
+        init_if_needed,
+        payer=payer,
+        seeds=[b"collection_token_account".as_ref()],
+        bump,
+        token::mint=collection_mint,
+        token::authority=collection_mint
+      )]
+
+
+     pub collection_token_account:Box<InterfaceAccount<'info,TokenAccount>>,
+
+
+
+    pub associated_token_program:Program<'info,AssociatedToken>,
+    pub token_metadata_program:Program<'info,Metadata>,
+   pub system_program:Program<'info,System>,
+   pub token_program:Interface<'info,TokenInterface>,
+     pub rent:Sysvar<'info,Rent>,
+
+
+}
 
 
 
